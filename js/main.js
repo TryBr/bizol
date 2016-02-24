8a201abac409899906eb9fe643c5cd3e6058ac2d
TryBr <avulsed.jb@gmail.com>
2016-02-25 01:03:55 +0500

    Всё сделано кроме выпадающих областей. Нужно их наполнить.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-
+$('.plus-block-1, .plus-block-2, .plus-block-3').hide();
+$('.plus__btn__1').click(function(){
+	if ($(this).hasClass('plus__btn')) {
+		$('.plus__btn__1').addClass('minus__btn');
+		$('.plus__btn__1').removeClass('plus__btn');
+    	$('.plus-block-1').slideToggle(500);
+	} else
+	{
+		$('.plus__btn__1').removeClass('minus__btn');
+		$('.plus__btn__1').addClass('plus__btn');
+	    $('.plus-block-1').slideToggle(500);
+    }
+});
+$('.plus__btn__2').click(function(){
+	if ($(this).hasClass('plus__btn')) {
+		$('.plus__btn__2').addClass('minus__btn');
+		$('.plus__btn__2').removeClass('plus__btn');
+    	$('.plus-block-2').slideToggle(500);
+	} else
+	{
+		$('.plus__btn__2').removeClass('minus__btn');
+		$('.plus__btn__2').addClass('plus__btn');
+	    $('.plus-block-2').slideToggle(500);
+    }
+});
+$('.plus__btn__3').click(function(){
+	if ($(this).hasClass('plus__btn')) {
+		$('.plus__btn__3').addClass('minus__btn');
+		$('.plus__btn__3').removeClass('plus__btn');
+    	$('.plus-block-3').slideToggle(500);
+	} else
+	{
+		$('.plus__btn__3').removeClass('minus__btn');
+		$('.plus__btn__3').addClass('plus__btn');
+	    $('.plus-block-3').slideToggle(500);
+    }
+});
